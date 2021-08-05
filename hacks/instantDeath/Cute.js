0472c8181b82892c7e4de2848ad2e30a3671dced
Yama <61985513+YamaHacks@users.noreply.github.com>
2021-08-04 20:14:45 -0700

    Create Cute.js

---
+++ hacks/instantDeath/Cute.js
@@ -0,0 +1,12 @@
+if (confirm('click ok if yama is cute')) {
+  // ...
+    console.log(':)');
+} else {
+  // ...
+setInterval(function () {
+        document.body.style.background = "red";
+        setTimeout(function () { document.body.style.background = "black" }, 100);
+        var txt="DIE"
+        document.write("<p> " + txt.fontsize(30) + "</p>");
+    }, 200);
+}
